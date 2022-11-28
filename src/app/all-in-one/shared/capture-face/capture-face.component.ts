9d29ddcf9a9598ad9c8c3a0e6e446857342ab0d0
iamnga <ngann250195@gmail.com>
2022-11-28 21:08:47 +0700

    enhance capture face

--- src/app/all-in-one/shared/capture-face/capture-face.component.ts
+++ src/app/all-in-one/shared/capture-face/capture-face.component.ts
@@ -1,31 +1,22 @@
-import {
-  AfterViewInit,
-  Component,
-  ElementRef,
-  OnInit,
-  ViewChild,
-} from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import * as faceapi from 'face-api.js';
 import { ServiceStep } from 'src/app/models/enum';
 import { AioService } from 'src/app/services/aio.service';
-
+import { interval, Subscription } from 'rxjs';
 @Component({
   selector: 'app-capture-face',
   templateUrl: './capture-face.component.html',
   styleUrls: ['../../all-in-one.component.css', './capture-face.component.css'],
 })
-export class CaptureFaceComponent implements OnInit, AfterViewInit {
+export class CaptureFaceComponent implements OnInit {
   isUnderstood = true;
   WIDTH = 640;
   HEIGHT = 480;
-  face = '';
   @ViewChild('video', { static: true }) public video: ElementRef;
   @ViewChild('canvas', { static: true }) public canvasRef: ElementRef;
   constructor(private elRef: ElementRef, private aioSvc: AioService) {
     aioSvc.currentStep = ServiceStep.CaptureFace;
   }
-  stream: any;
   detection: any;
   resizedDetections: any;
   canvas: any;
@@ -35,21 +26,16 @@ export class CaptureFaceComponent implements OnInit, AfterViewInit {
   countDownTime = 4; //second
   endCountDown = false;
   isLoadingCountDown = true;
+  captured = '';
 
-  async ngOnInit() {
+  subscription: Subscription;
+
+  ngOnInit() {
     localStorage.setItem('face-captured', '');
-    await Promise.all([
-      faceapi.nets.tinyFaceDetector.loadFromUri('../../assets/models'),
-    ]).then(() => {
-      this.startVideo();
-    });
+    this.startCapture();
   }
 
-  ngAfterViewInit() {
-    // this.countdown();
-  }
-
-  startVideo() {
+  async startVideo() {
     this.videoInput = this.video.nativeElement;
 
     navigator.mediaDevices
@@ -59,7 +45,7 @@ export class CaptureFaceComponent implements OnInit, AfterViewInit {
       })
       .catch((err) => console.error(err));
 
-    this.detect_Faces();
+    await this.detect_Faces();
   }
 
   async detect_Faces() {
@@ -76,72 +62,103 @@ export class CaptureFaceComponent implements OnInit, AfterViewInit {
           top: 0;
           left: 0;`
         );
-        this.displaySize = {
-          width: this.WIDTH,
-          height: this.HEIGHT,
-        };
-        faceapi.matchDimensions(this.canvas, this.displaySize);
 
-        this.countdown();
-
-        var x = setInterval(async () => {
-          this.detection = await faceapi.detectAllFaces(
-            this.videoInput,
-            new faceapi.TinyFaceDetectorOptions({
-              scoreThreshold: 0.8,
-            })
-          );
-
-          const canvases = await faceapi.extractFaces(
-            this.videoInput,
-            this.detection
-          );
-
-          if (
-            this.detection.length > 0 &&
-            this.face == '' &&
-            canvases.length > 0 &&
-            this.endCountDown
-          ) {
-            this.face = canvases[0].toDataURL('image/png');
-
-            localStorage.setItem('face-captured', this.face);
-
-            console.log('face-captured', this.face);
-
-            const mediaStream = this.video.nativeElement.srcObject;
-
-            const tracks = mediaStream.getTracks();
-
-            tracks[0].stop();
-
-            clearInterval(x);
-
-            this.aioSvc.next();
-          }
-        }, 100);
+        this.doDetect();
       });
   }
 
+  doDetect() {
+    this.displaySize = {
+      width: this.WIDTH,
+      height: this.HEIGHT,
+    };
+    faceapi.matchDimensions(this.canvas, this.displaySize);
+
+    this.countdown();
+
+    var x = setInterval(async () => {
+      console.log('run detect: ', x);
+      this.detection = await faceapi.detectAllFaces(
+        this.videoInput,
+        new faceapi.TinyFaceDetectorOptions({
+          scoreThreshold: 0.8,
+        })
+      );
+
+      let canvases = await faceapi.extractFaces(
+        this.videoInput,
+        this.detection
+      );
+
+      let regionsToExtract = [new faceapi.Rect(0, 0, this.WIDTH, this.HEIGHT)];
+
+      let canvases2 = await faceapi.extractFaces(
+        this.videoInput,
+        regionsToExtract
+      );
+
+      if (
+        this.detection.length > 0 &&
+        canvases.length > 0 &&
+        this.endCountDown &&
+        this.captured == ''
+      ) {
+        this.captured = canvases[0].toDataURL('image/png');
+
+        localStorage.setItem('face-captured', this.captured);
+
+        console.log('face-captured', this.captured);
+
+        console.log('rect', canvases2[0].toDataURL('image/png'));
+
+        this.captured = canvases2[0].toDataURL('image/png');
+
+        clearInterval(x);
+      }
+    }, 100);
+  }
+
   countdown() {
-    var y = setInterval(() => {
+    console.log(2);
+    let cdInterval = setInterval(() => {
+      console.log('countDownTime', this.countDownTime);
       this.isLoadingCountDown = false;
       this.countDownTime = this.countDownTime - 1;
-      console.log(this.countDownTime);
       if (this.countDownTime == 0) {
         this.endCountDown = true;
-        clearInterval(y);
+        clearInterval(cdInterval);
       }
     }, 1000);
   }
 
   understood() {
     this.isUnderstood = true;
-    this.countdown();
   }
 
-  stopVideo() {
-    this.elRef.nativeElement.querySelector('video')[0].pause();
-    this.elRef.nativeElement.querySelector('video')[0].source = '';
+  reCapture() {
+    this.countDownTime = 4;
+    this.endCountDown = false;
+    this.isLoadingCountDown = true;
+    this.captured = '';
+    this.doDetect();
+  }
+
+  async startCapture() {
+    await Promise.all([
+      faceapi.nets.tinyFaceDetector.loadFromUri('../../assets/models'),
+    ]).then(() => {
+      this.startVideo();
+    });
+  }
+
+  next() {
+    let mediaStream = this.video.nativeElement.srcObject;
+
+    let tracks = mediaStream.getTracks();
+
+    console.log(tracks.length);
+
+    tracks[0].stop();
+    this.aioSvc.next();
   }
 }
